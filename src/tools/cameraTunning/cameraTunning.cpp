1567a54ba99a0a736b4e05073f792bd876fce642
gcjurgiel <gcjurgiel@icloud.com>
2020-01-26 19:31:02 -0600

    Added sliders to camera tunning

--- src/tools/cameraTunning/cameraTunning.cpp
+++ src/tools/cameraTunning/cameraTunning.cpp
@@ -19,6 +19,20 @@ int main() {
   namedWindow(rawWindow, WINDOW_NORMAL);
   namedWindow(rawWindow, WINDOW_NORMAL);
 
+  int hLow = 60;
+  int hHigh = 70;
+  int sLow = 220; 
+  int sHigh = 255;
+  int lLow = 100;
+  int lHigh = 140;
+
+  createTrackbar( "Hue Low", threshWindow, &hLow, 180, on_trackbar );
+  createTrackbar( "Hue High", threshWindow, &hHigh, 180, on_trackbar );
+  createTrackbar( "Saturation Low", threshWindow, &sLow, 255, on_trackbar );
+  createTrackbar( "Saturation High", threshWindow, &sHigh, 255, on_trackbar );
+  createTrackbar( "Lightness Low", threshWindow, &lLow, 255, on_trackbar );
+  createTrackbar( "Lightness High", threshWindow, &lHigh, 255, on_trackbar );
+
   Mat rawFrame, threshFrame;
   while(true) {
     capture >> rawFrame;
@@ -29,7 +43,11 @@ int main() {
       break;
     }
 
+    cvtColor(rawFrame, threshFrame, CV_BGR2HLS);
+    inRange(hsv, Scalar(hLow, lLow, sLow), Scalar(hHigh, lHigh, sHigh), threshFrame);
+
     imshow(rawWindow, rawFrame);
+    imshow(threshWindow, threshFrame);
 
     if (waitKey(10) == 27) {
       cout << "Esc pressed. Capture ended" << endl;
